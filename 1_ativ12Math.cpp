e0c3d16871cbd5b2bb2bfbcf437a6497cf18584e
Leticia Azevedo <letgis@br.ibm.com>
2021-04-11 17:47:45 -0300

    Save changes

--- 1_ativ12Math.cpp
+++ 1_ativ12Math.cpp
@@ -4,9 +4,30 @@ coordenadas (x,y) de cada ponto. Faça um programa que resolva
 este problema para o engenheiro, sabendo que o cálculo da
 distância é dado por:
 
-         d = raiz quadrada (x²−x¹)² + (y² − y¹)² 
+         d = raiz quadrada (x²−x¹)² + (y² − y¹)² ||||| d é igual raiz quadrada de x2 - x1 + y2 - y1 ao quadrado
 Dica: Use sqrt() (math.h) para calcular a raiz quadrada.*/
 
 #include <stdio.h>
 #include <iostream>
+#include <math.h>
 
+int main(){
+    float x1, y1, x2, y2, difx, dify, quaddifx, quaddify, dist;
+
+    printf("x2? | x1? | y2? | y1?\n");
+    scanf("%f", &x2);
+    scanf("%f", &x1);
+    scanf("%f", &y2);
+    scanf("%f", &y1);
+
+    difx = x2 - x1;
+    dify = y2 - y1;
+
+
+    quaddifx = difx * difx;
+    quaddify = dify * dify;
+    dist = sqrt(quaddifx + quaddify);
+
+    printf("\n%.2f\n", dist);
+
+}
